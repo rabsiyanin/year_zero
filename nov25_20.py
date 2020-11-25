c4c036c42d22b3bda829d7647436e6d4f736852d
Georgii <75013070+rabsiyanin@users.noreply.github.com>
2020-11-25 12:34:35 +0300

    Update nov25_20.py

--- nov25_20.py
+++ nov25_20.py
@@ -13,7 +13,7 @@ if A == B:
 
 #задание 2
 A = int(input('Число А:'))
-B = int(input('Число B >> '))
+B = int(input('Число B: '))
 for i in range(A, B + 1):
     if (i // 1000) == i % 10 and (i // 100) % 10 == (i // 10) % 10:
         print(i)  
